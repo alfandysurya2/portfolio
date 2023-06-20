5c4561d7f69b1c336259c3fc5cf4b8922748b493
alfandysurya2 <alfandy.surya3@gmail.com>
2023-06-20 23:30:25 +0700

    32nd update: updating dags script

--- E-Commerce Data Pipeline with Airflow Project/dags/amazon_sale_report_dags.py
+++ E-Commerce Data Pipeline with Airflow Project/dags/amazon_sale_report_dags.py
@@ -3,7 +3,14 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+from plugins import amazon_sale_report_function as sr
 import pandas as pd
+
+# Function to Convert Pulled data from Postgres into pandas DataFrame
+def convert_to_df(**context):
+    data = context['task_instance'].xcom_pull(task_ids='postgre_data_pull')
+    df = pd.DataFrame(data)
+    return df
 
 # Define the DAG
 dag = DAG(
@@ -13,13 +20,6 @@ dag = DAG(
     start_date=datetime(2023, 1, 1),
     catchup=False
 )
-
-# Define preprocessing function
-def process_data(**context):
-    data = context['ti'].xcom_pull(task_ids='postgre_data_pull')
-    df = pd.DataFrame(data, columns=['column1', 'column2', 'column3'])
-    return 
-
 
 # Define tasks
 start_task = EmptyOperator(task_id='start_task', dag=dag)
@@ -33,15 +33,31 @@ pull_data_task = PostgresOperator(
     dag=dag
 )
 
-process_data_task = PythonOperator(
-    task_id='process_data',
-    python_callable=process_data,
+# Task to convert pulled data from PostgreSQL to Pandas Dataframe
+convert_to_df_task = PythonOperator(
+    task_id='convert_to_df',
+    python_callable=convert_to_df,
+    dag=dag
+)
+
+column_standardization = PythonOperator(
+    task_id='column_standardization_task',
+    python_callable=sr.column_standardization,
     provide_context=True,
+    op_kwargs={'df':"{{ task_instance.xcom_pull(task_ids='postgre_data_pull') }}"},
+    dag=dag
+)
+
+process_column = PythonOperator(
+    task_id='column_processing_task',
+    python_callable=sr.process_column,
+    provide_context=True,
+    op_kwargs={'df':"{{ task_instance.xcom_pull(task_ids='postgre_data_pull') }}"},
     dag=dag
 )
 
 # Task dependencies
-start_task >> pull_data_task
+start_task >> pull_data_task >> column_standardization
 
 
 
