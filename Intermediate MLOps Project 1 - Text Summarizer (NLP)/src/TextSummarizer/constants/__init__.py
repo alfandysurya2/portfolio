cb3991d1993af07ef39037df0aceba97c7a1f530
alfandysurya2 <alfandy.surya3@gmail.com>
2023-08-24 15:15:57 +0700

    add data_validation.ipynb:

--- Intermediate MLOps Project 1 - Text Summarizer (NLP)/src/TextSummarizer/constants/__init__.py
+++ Intermediate MLOps Project 1 - Text Summarizer (NLP)/src/TextSummarizer/constants/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
 
-CONFIG_FILE_PATH = Path('config/config.yaml')
-PARAMS_FILE_PATH = Path('params.yaml')
+current_dir = os.getcwd()
+
+CONFIG_FILE_PATH = Path(os.path.join(current_dir, 'config/config.yaml'))
+PARAMS_FILE_PATH = Path(os.path.join(current_dir, 'params.yaml'))
