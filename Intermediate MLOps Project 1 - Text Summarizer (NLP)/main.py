1651085f14cf61ce3ba9f74a0a504178de7d8e05
alfandysurya2 <alfandy.surya3@gmail.com>
2023-08-24 10:56:19 +0700

    new update

--- Intermediate MLOps Project 1 - Text Summarizer (NLP)/main.py
+++ Intermediate MLOps Project 1 - Text Summarizer (NLP)/main.py
@@ -1,3 +1,13 @@
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 
-logger.info("Custom Logging")
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f"----- {STAGE_NAME} Started -----")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"----- {STAGE_NAME} Completed -----\n\n================================")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
